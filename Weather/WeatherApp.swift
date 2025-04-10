e927c747387d8648cf5361ecc7a1b4d2a9ee2361
yerassyl-saiman <yerassyl.s@1fit.app>
2025-04-10 20:15:29 +0500

    [feat] add project base

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Yerassyl Saiman on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
