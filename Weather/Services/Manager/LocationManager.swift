b2b26e0b414aca754af9acd00bb655e6a995382c
yerassyl-saiman <yerassyl.s@1fit.app>
2025-04-10 23:43:14 +0500

    [feat] add location tracker

---
+++ Weather/Services/Manager/LocationManager.swift
@@ -0,0 +1,48 @@
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    
+    @Published var lastLocation: CLLocation?
+    @Published var cityName: String = ""
+    @Published var locationError: Error?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        // Обратное геокодирование для получения названия города
+        CLGeocoder().reverseGeocodeLocation(location) { [weak self] placemarks, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    self?.locationError = error
+                }
+                return
+            }
+            if let city = placemarks?.first?.locality {
+                DispatchQueue.main.async {
+                    self?.cityName = city
+                    self?.locationError = nil // очищаем ошибку, если определение прошло успешно
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self?.locationError = NSError(domain: "LocationError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Не удалось определить город"])
+                }
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            self.locationError = error
+        }
+    }
+}
